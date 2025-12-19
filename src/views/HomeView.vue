7fd7b9f7800f9f0c9ffa3bd42dfcc4cbadfe1f34
Karolina Berg <karolina@karolinaberg.com>
2025-12-19 11:16:24 +0100

    Städa upp och formatera HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
-  <section>
-    <h1>Hem</h1>
-    <p>Välkommen till Bil & Däckservice.</p>
-  </section>
+  <div class="page-container">
+    <div class="page-header">
+      <h1>Välkommen till Bil & Däckservice</h1>
+      <p>Din pålitliga partner för allt som rör din bil.</p>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+</style>
